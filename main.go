b473cb54d12d06f1ba778f24e0c864288e804394
ocurr <owcurry42@gmail.com>
2016-10-09 22:12:28 -0700

    initial commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"github.com/jung-kurt/gofpdf"
+)
+
+func main() {
+	var inImg string
+	var outPdf string
+
+	flag.StringVar(&inImg, "i", "", "The image to be converted")
+	flag.StringVar(&outPdf, "o", "", "The name of the pdf that will be generated,\n\t if no name is specified the name will match that of the input image")
+	flag.Parse()
+
+	if inImg == "" {
+		flag.PrintDefaults()
+		return
+	}
+
+	outPdf = inImg[:(len(inImg)-4)] + ".pdf"
+
+	pdf := gofpdf.New("P", "mm", "A4", "")
+	pdf.SetMargins(10, 10, 10)
+	pdf.AddPage()
+	_, pgWidth := pdf.GetPageSize()
+	img := pdf.RegisterImage(inImg, "")
+	offset := (pgWidth - img.Width()) / 3.5
+	pdf.ImageOptions(inImg, offset, 10, 0, 0, true, gofpdf.ImageOptions{ReadDpi: true}, 0, "")
+	pdf.OutputFileAndClose(outPdf)
+}
